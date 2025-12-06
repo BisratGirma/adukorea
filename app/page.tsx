3e1cea79dd29668c2cf254cc3759c59094afb0e6
Bisrat <bisratgirma36@gmail.com>
2025-12-06 23:31:02 +0300

    Refactor Home and Hero components; simplify layout by removing background wrapper and moving background styles to Hero

--- app/page.tsx
+++ app/page.tsx
@@ -16,22 +16,8 @@ export default function Home() {
 
   return (
     <main>
-      {/* Background wrapper for nav + hero */}
-      <div className="relative">
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{
-            backgroundImage:
-              "url('https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80')",
-          }}
-        />
-        <div className="absolute inset-0 bg-black/40" />
-
-        <div className="relative z-10">
-          <Navigation transparent />
-          <Hero />
-        </div>
-      </div>
+      <Navigation />
+      <Hero />
 
       <ProductCarousel category="K-Beauty / Skin Care" products={kBeautyProducts} />
       <ProductCarousel category="Phones/ Tablets" products={phoneProducts} />
